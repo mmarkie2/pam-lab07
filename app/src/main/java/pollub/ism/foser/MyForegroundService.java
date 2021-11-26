e2864b29983e440eb75277ef6bad9f1be0c96b06
mm <maciej.markiewicz10@gmail.com>
2021-11-26 19:02:20 +0100

    lab8 final

--- app/src/main/java/pollub/ism/foser/MyForegroundService.java
+++ app/src/main/java/pollub/ism/foser/MyForegroundService.java
@@ -1,11 +1,18 @@
 package pollub.ism.foser;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 public class MyForegroundService extends Service {
     //1. Kanał notyfikacji
@@ -37,6 +44,7 @@ public class MyForegroundService extends Service {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //return super.onStartCommand(intent, flags, startId);
@@ -45,6 +53,23 @@ public class MyForegroundService extends Service {
         show_time = intent.getBooleanExtra(TIME,false);
         do_work = intent.getBooleanExtra(WORK,false);
         double_speed = intent.getBooleanExtra(WORK_DOUBLE,false);
+
+        createNotificationChannel();
+
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+
+
+        Notification notification = new Notification.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_my_icon)
+                .setContentTitle(getString(R.string.ser_title))
+                .setShowWhen(show_time)
+                .setContentText(message)
+                .setLargeIcon(BitmapFactory.decodeResource (getResources() , R.drawable.circle ))
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1,notification);
 
         doWork();
 
@@ -58,5 +83,12 @@ public class MyForegroundService extends Service {
                 +"\n double_speed=" + double_speed.toString();
 
         Toast.makeText(this, info ,Toast.LENGTH_LONG).show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+        NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(serviceChannel);
     }
 }
